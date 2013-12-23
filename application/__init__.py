dd67f98accd772664efd53b07ed117e66b48dc58
patrick <patrickjvarin@gmail.com>
2013-12-22 21:26:25 -0500

    webpage working

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,26 @@
 import os
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/')
+app.debug=True
+
+# setup routes
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route("/index")
+@app.route("/")
 def index():
-	return render_template('index.html')
-#import application.models
-#import application.views
-#import application.controllers
+    return render_template('index.html')
+
+@app.route("/ideas")
+@app.route("/portfolio")
+@app.route("/about")
+def comming_soon():
+	return render_template('comingsoon.html')
